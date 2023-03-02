e89a138e4f55fd3a15b30690fdfab0c32639bcdf
Michael <michael.mena@ramq.gouv.qc.ca>
2023-03-02 16:16:31 -0500

    Mctasks - First class

--- scripts/Mctasks.js
+++ scripts/Mctasks.js
@@ -1,9 +1,21 @@
+
 class Mctasks{
 
-    Mctasks(id, name, description){
+    constructor(id, name, description, date, hour){
         this.id = id;
         this.name = name;
         this.description = description;
+        this.date = date;
+        this.hour = hour;
+    }
+
+    get name(){
+        return "The name of the tasks is " + this.name;
+    }
+
+    set name(nameParameter){
+        this.name = nameParameter;
+        return "The name was changed for "+ nameParameter;
     }
 
 
