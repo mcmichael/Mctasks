66164e991fe9b79bd49d08965f9f60085218251c
Michael <michael.mena@gmail.com>
2023-03-06 21:04:02 -0500

    Base objects 3

--- scripts/McLists.js
+++ scripts/McLists.js
@@ -1,1 +1,3 @@
-const Lists = ["Planing project","Guide","Diet"];
+const McLists = ["Planing project","Working out plan","Study guide","Brainstorming","Diet","Healthy plan",
+"Personal finances","Savings","Stocks","Find appartment","Cleaning","Planning trip","Trip reservation","Concert","Camplng",
+"Family education","Planing saving for education"];
